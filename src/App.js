c491db87e35a2ce758b0a4600ee686156ecbcef4
Ali-hd <ali.hamidaddin1@gmail.com>
2019-11-01 12:03:06 +0300

    try

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css'
+import Navigate from './components/Navigate'
+
+export default class App extends Component {
+
+  state={
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigate/>
+      </div>
+    )
+  }
+}
+
---
+++ src/components/Navigate.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, ButtonToolbar, Modal } from 'react-bootstrap';
+
+const Navigate = (props) => {
+    return (
+        <div>
+            <h1>Hello navigation</h1>
+            <ButtonToolbar>
+                <Button variant="outline-dark">Dark</Button>
+            </ButtonToolbar>
+        </div>
+    )
+}
+
+export default Navigate

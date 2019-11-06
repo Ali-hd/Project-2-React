125c3c6bc540493012e1d0b900e209aac4a20446
Ali-hd <ali.hamidaddin1@gmail.com>
2019-11-06 16:30:42 +0300

    finished

--- src/components/ErrorPath.jsx
+++ src/components/ErrorPath.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ErrorPath = () => {
     return (
-        <div style={{marginTop: '10%', marginLeft: '30%'}}>
-            <iframe src="https://i.imgur.com/3LQNyXn.gif" width="960" height="500" frameBorder="0"></iframe>
-            <div style={{marginLeft: '10%'}}>
+        <div style={{marginTop:'10%',textAlign:'center'}}>
+            <img src="https://i.imgur.com/3LQNyXn.gif" width="960" height="500" frameBorder="0"/>
+            <div>
             <h1>ERROR 404</h1>
            <h2>Page not found..</h2>
             </div>
